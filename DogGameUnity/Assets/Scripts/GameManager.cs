46512a4a3665a5a41012e4dfbb09ab0ea8396919
Go <giwigi@naver.com>
2021-11-10 15:02:08 +0900

    범위 넘어갔을 때 위치 원래대로 돌아가게 함과 가격 설정

--- DogGameUnity/Assets/Scripts/GameManager.cs
+++ DogGameUnity/Assets/Scripts/GameManager.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public int[] jellyGoldList;
+
     public Vector3[] PointList;
+
+    public RuntimeAnimatorController[] LevelAc;
+
+
+    public void ChangeAc(Animator anim, int level)
+    {
+        anim.runtimeAnimatorController = LevelAc[level - 1];
+    }
 }
