abf9a3d65e31fda299fcab62f2844cb0e8af1523
Go <giwigi@naver.com>
2021-11-23 13:45:24 +0900

    Update AssetImportWorker0.log

--- DogGameUnity/Assets/Scripts/GameManager.cs
+++ DogGameUnity/Assets/Scripts/GameManager.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public Sprite[] jellySpriteList;
+    public string[] jellyNameList;
+    public int[] jellyJelatinList;
     public int[] jellyGoldList;
 
     public Vector3[] PointList;
 
     public RuntimeAnimatorController[] LevelAc;
 
-
     public void ChangeAc(Animator anim, int level)
     {
         anim.runtimeAnimatorController = LevelAc[level - 1];
     }
+
 }
