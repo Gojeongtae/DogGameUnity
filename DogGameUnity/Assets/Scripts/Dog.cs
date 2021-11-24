7e0b11d6de3f3017d8f76db75cfbd3655738ac63
Go <giwigi@naver.com>
2021-11-24 15:13:44 +0900

    강아지 데이터값 가져오기

---
+++ DogGameUnity/Assets/Scripts/Dog.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog
+{
+    public int ID;
+    public int Level;
+    public float Exp;
+
+    public Dog(int ID, int Level, float Exp)
+    {
+        this.ID = ID;
+
+        this.Level = Level;
+
+        this.Exp = Exp;
+    }
+
+}
